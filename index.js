6186f87b65dece9816d93ac5beef63eea7620df5
David <dgunther27@cpatech.org>
2025-11-21 14:45:31 -0500

    Added images

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function flipCard(id){
     let card = document.getElementById(id);
     let image = document.createElement("img");
-    image.src = "resources/cards/bearcard.png";
-    card.appendChild(image)
+    image_list = [url('bearcard.png'), url('beavercard.png'), url('deercard.png'), url('foxcard.png'), url('hedgehogcard.png'), url('lynxcard.png'), url('moosecard.png'), url('raccooncard.png'), url('squirrelcard.png'), url('wolfcard.png')];
+    image.src = "resources/cards/image_list";
+    card.appendChild(image);
 }
 
 function restartbutton(){

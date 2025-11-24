01a5c81e573b18f9177d535fe60e9362b37a6dd5
David <dgunther27@cpatech.org>
2025-11-24 14:07:05 -0500

    Updated Image Source JS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function flipCard(id){
     const image_list = ['bearcard.png', 'beavercard.png', 'deercard.png', 'foxcard.png', 'hedgehogcard.png', 'lynxcard.png', 'moosecard.png', 'raccooncard.png', 'squirrelcard.png', 'wolfcard.png'];
     for (let i=0; i < image_list.length; i++){
         let random_image = Math.floor(Math.random()*image_list.length);
-        image.src = `resources/cards/${random_image}`;
+        image.src = `resources/cards/${image_list[random_image]}`;
         card.appendChild(image);
     }
 }

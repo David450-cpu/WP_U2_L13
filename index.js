824fe1fc554b01544e7ef1ea5ab0809663e3313b
David <dgunther27@cpatech.org>
2025-11-24 12:49:19 -0500

    Edited for Loop

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function flipCard(id){
     let card = document.getElementById(id);
     let image = document.createElement("img");
     const image_list = ["url('bearcard.png')", url('beavercard.png'), url('deercard.png'), url('foxcard.png'), url('hedgehogcard.png'), url('lynxcard.png'), url('moosecard.png'), url('raccooncard.png'), url('squirrelcard.png'), url('wolfcard.png')];
-    for (let i=0; i == 20; i++){
+    for (let i=0; i < image_list.length; i++){
         let random_image = Math.floor(Math.random()*image_list.length);
         let image.src = "resources/cards/random_image";
         card.appendChild(image);

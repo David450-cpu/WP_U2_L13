742bb1c12634e811b60ac151fda932ce983e5f78
David <dgunther27@cpatech.org>
2025-11-24 14:45:12 -0500

    Made Images Appear Only Twice

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+const image_list = ['bearcard.png', 'beavercard.png', 'deercard.png', 'foxcard.png', 'hedgehogcard.png', 'lynxcard.png', 'moosecard.png', 'raccooncard.png', 'squirrelcard.png', 'wolfcard.png', 'bearcard.png', 'beavercard.png', 'deercard.png', 'foxcard.png', 'hedgehogcard.png', 'lynxcard.png', 'moosecard.png', 'raccooncard.png', 'squirrelcard.png', 'wolfcard.png'];
+
 function flipCard(id){
     let card = document.getElementById(id);
     let image = document.createElement("img");
-    const image_list = ['bearcard.png', 'beavercard.png', 'deercard.png', 'foxcard.png', 'hedgehogcard.png', 'lynxcard.png', 'moosecard.png', 'raccooncard.png', 'squirrelcard.png', 'wolfcard.png'];
-    for (let i=0; i < image_list.length; i++){
-        let random_image = Math.floor(Math.random()*image_list.length);
-        image.src = `resources/cards/${image_list[random_image]}`;
-        card.appendChild(image);
-        card.setAttribute("onclick", "none");
-    }
+    let random_image = Math.floor(Math.random()*image_list.length);
+    image.src = `resources/cards/${image_list[random_image]}`;
+    card.appendChild(image);
+    card.setAttribute("onclick", "none");
+    image_list.splice(random_image, 1);
 }
 
 function restartbutton(){

020e518b458074337375ae0addf277a31038a036
David <dgunther27@cpatech.org>
2025-11-24 12:42:11 -0500

    Updated JavaScript

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function flipCard(id){
     let card = document.getElementById(id);
     let image = document.createElement("img");
-    image_list = [url('bearcard.png'), url('beavercard.png'), url('deercard.png'), url('foxcard.png'), url('hedgehogcard.png'), url('lynxcard.png'), url('moosecard.png'), url('raccooncard.png'), url('squirrelcard.png'), url('wolfcard.png')];
-    image.src = "resources/cards/image_list";
-    card.appendChild(image);
+    const image_list = ["url('bearcard.png')", url('beavercard.png'), url('deercard.png'), url('foxcard.png'), url('hedgehogcard.png'), url('lynxcard.png'), url('moosecard.png'), url('raccooncard.png'), url('squirrelcard.png'), url('wolfcard.png')];
+    for (let i=0; i == 20; i++){
+        let random_image = Math.floor(Math.random()*image_list.length);
+        let image.src = `resources/cards/${random_image}`;
+        card.appendChild(image);
+    }
 }
 
 function restartbutton(){

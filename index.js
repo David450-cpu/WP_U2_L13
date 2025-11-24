245b994589dd1d51b3a4c7b204f4506e61cb5ba2
David <dgunther27@cpatech.org>
2025-11-24 12:46:04 -0500

    Removed F-String in JS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function flipCard(id){
     const image_list = ["url('bearcard.png')", url('beavercard.png'), url('deercard.png'), url('foxcard.png'), url('hedgehogcard.png'), url('lynxcard.png'), url('moosecard.png'), url('raccooncard.png'), url('squirrelcard.png'), url('wolfcard.png')];
     for (let i=0; i == 20; i++){
         let random_image = Math.floor(Math.random()*image_list.length);
-        let image.src = `resources/cards/${random_image}`;
+        let image.src = "resources/cards/random_image";
         card.appendChild(image);
     }
 }

421d73eff0289852f62f8b88f702ad02ba6df43d
David <dgunther27@cpatech.org>
2025-11-25 14:13:55 -0500

    Improved JavaScript in General

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const image_list = ['bearcard.png', 'beavercard.png', 'deercard.png', 'foxcard.png', 'hedgehogcard.png', 'lynxcard.png', 'moosecard.png', 'raccooncard.png', 'squirrelcard.png', 'wolfcard.png', 'bearcard.png', 'beavercard.png', 'deercard.png', 'foxcard.png', 'hedgehogcard.png', 'lynxcard.png', 'moosecard.png', 'raccooncard.png', 'squirrelcard.png', 'wolfcard.png'];
+let next_up = []
 
 function flipCard(id){
     let card = document.getElementById(id);
@@ -7,23 +8,27 @@ function flipCard(id){
     image.src = `resources/cards/${image_list[random_image]}`;
     card.appendChild(image);
     card.setAttribute("onclick", "none");
+    next_up.push(random_image)
     image_list.splice(random_image, 1);
-
-    /* if (somehow check if 2 cards are flipped){ */
-       /* checkForMatch(); } */
+    if (next_up.length == 2){
+       checkForMatch();
+    }
 }
 
-function checkForMatch(){   /* somehow change random_image var to the first 2 flipped cards. random_image cant go to this function */
-    if (random_image !== random_image){
-        card.setAttribute("image", "none")
+function checkForMatch(){
+    console.log(next_up)
+    if (next_up[0] !== next_up[1]){
+        console.log("Not a match")
+        card.setAttribute("image", "none");
+        let next_up = []
     }
-
-    if (random_image === random_image){
-        match = true;
+    if (next_up[0] === next_up[1]){
+        console.log("Is a match")
+        let match = true;
+        let next_up = []
         if (match = true){
-            /* cards stay flipped somehow */
+            // cards WILL stay flipped
         }
-    
     }
 }
 

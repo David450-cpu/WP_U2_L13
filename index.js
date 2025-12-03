869c71ae93badfbd14d027663374a2cfa047d222
David <dgunther27@cpatech.org>
2025-12-03 12:47:58 -0500

    Edited JS If Statements

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function flipCard(id){
 
 function checkForMatch(){
     console.log(next_up)
-    if (next_up[0] !== next_up[1]){
+    if (`../resources/cards/${next_up[0]}` !== `../resources/cards/${next_up[1]}`){
         console.log("Not a match")
     }
     if (next_up[0] === next_up[1]){

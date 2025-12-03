d8a5ef8790b2c58bb471312b02ec7a94fcca0b6a
David <dgunther27@cpatech.org>
2025-12-03 14:07:35 -0500

    Added Semi-Colons to JS

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const image_list = ['bearcard.png', 'beavercard.png', 'deercard.png', 'foxcard.png', 'hedgehogcard.png', 'lynxcard.png', 'moosecard.png', 'raccooncard.png', 'squirrelcard.png', 'wolfcard.png', 'bearcard.png', 'beavercard.png', 'deercard.png', 'foxcard.png', 'hedgehogcard.png', 'lynxcard.png', 'moosecard.png', 'raccooncard.png', 'squirrelcard.png', 'wolfcard.png'];
-let next_up = []
+let next_up = [];
 
 function flipCard(id){
     let card = document.getElementById(id);
@@ -8,7 +8,7 @@ function flipCard(id){
     image.src = `resources/cards/${image_list[random_image]}`;
     card.appendChild(image);
     card.setAttribute("onclick", "none");
-    next_up.push(card)
+    next_up.push(card);
     image_list.splice(random_image, 1);
     if (next_up.length == 2){
        checkForMatch();
@@ -16,21 +16,21 @@ function flipCard(id){
 }
 
 function checkForMatch(){
-    console.log(next_up)
+    console.log(next_up);
     if (`resources/cards/${next_up[0]}` !== `resources/cards/${next_up[1]}`){
-        console.log("Not a match")
+        console.log("Not a match");
     }
     if (`resources/cards/${next_up[0]}` === `resources/cards/${next_up[1]}`){
-        console.log("Is a match")
+        console.log("Is a match");
         let match = true;
         if (match = true){
             // Cards WILL stay flipped
         }
     }
-    next_up = []
-    console.log(next_up)
+    next_up = [];
+    console.log(next_up);
 }
 
 function restartbutton(){
-	window.location.reload()
+	window.location.reload();
 }

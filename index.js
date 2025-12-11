37464665035674ee92c26df16f18178db29bf87d
David <dgunther27@cpatech.org>
2025-12-11 12:41:07 -0500

    Added Win Text

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ function checkForMatch(){
     //next_up[0].children[0].scr
     let player1 = document.getElementsByClassName("p1score")[0];
     let player2 = document.getElementsByClassName("p2score")[0];
+    let wintext = document.getElementsByClassName("wins")[0];
     if (next_up[0].children[0].src === next_up[1].children[0].src){
         console.log("Is a match");
         let match = true;
@@ -55,15 +56,25 @@ function checkForMatch(){
             // Cards WILL stay flipped
             if (playerturn == "Player 1"){
                 player1score += 1;
-                sessionStorage.setItem("score", player1score);
+                sessionStorage.setItem("scoreP1", player1score);
                 turntext.textContent = "Player 1's turn!"
-                player1.textContent = `Player 1 score: ${player2score}`
+                player1.textContent = `Player 1 score: ${player1score}`
             }
             else if (playerturn == "Player 2"){
                 player2score += 1;
-                sessionStorage.setItem("score", player2score);
+                sessionStorage.setItem("scoreP2", player2score);
                 turntext.textContent = "Player 2's turn!"
                 player2.textContent = `Player 2 score: ${player2score}`
+            }
+            if (player1score + player2score == 10){
+                if (player1score > player2score){
+                    sessionStorage.setItem("P1wins", "Player 1 wins!");
+                    wintext.textContent = "Player 1 wins!"
+                }
+                else if (player2score > player1score){
+                    sessionStorage.setItem("P2wins", "Player 2 wins!");
+                    wintext.textContent = "Player 2 wins!"
+                }
             }
         }
     }
